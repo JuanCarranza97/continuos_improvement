6a51e8d8778ad2ab27b9d506807f96c0bbeb10c0
JuanCarranza97 <juan15290337@itcg.edu.mx>
2019-02-19 02:38:26 -0600

    Trash erase multiple scripts

--- multiple_scripts/concurrency_stress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#all_command = "xterm -e '{} ; echo $? > error.log' &".format(script
-xterm -e 'bash script1.sh ; echo $? > error.log' &
-pid=$!
-echo "Script was initializaded.. "
-wait $!
-echo "The error code was $?"
-echo "Script finished succesfully :)"
--- multiple_scripts/python_concurrency.py
+++
@@ -1,7 +0,0 @@
-import os,subprocess
-
-print("Preparing to run scripts")
-
-proc=subprocess.Popen("xterm -e 'bash script1.sh ; echo $? > error.log' &", shell=True)
-print("I catch {}".format(proc.pid))
-
